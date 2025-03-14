6f16361662456f362d3a4946e6ca40d892f8b064
ushpineva <ushpineva@ozon.ru>
2025-03-15 00:56:50 +0300

    fix session cleanup

--- src/main/java/itmo/blps/config/SessionCleanupListener.java
+++ src/main/java/itmo/blps/config/SessionCleanupListener.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpSessionEvent;
 import jakarta.servlet.http.HttpSessionListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -25,6 +26,7 @@ public class SessionCleanupListener implements HttpSessionListener {
 //        System.out.println("Session создана: " + se.getSession().getId());
 //    }
 
+    @Transactional
     @Override
     public void sessionDestroyed(HttpSessionEvent se) {
         String sessionId = se.getSession().getId();
