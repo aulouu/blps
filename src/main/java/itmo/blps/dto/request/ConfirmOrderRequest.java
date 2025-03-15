3187a28ec2682a1892a08529efac35cb86eac0e5
aulouu <alina.minkovva@gmail.com>
2025-03-15 12:47:20 +0300

    delivery time

--- src/main/java/itmo/blps/dto/request/ConfirmOrderRequest.java
+++ src/main/java/itmo/blps/dto/request/ConfirmOrderRequest.java
@@ -11,7 +11,6 @@ import java.time.LocalTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ConfirmOrderRequest {
-    @Pattern(regexp = "^([01]?[0-9]|2[0-3]):[0-5][0-9]$", message = "Delivery time must be in HH:mm format")
     private String deliveryTime;
     private Integer utensilsCount;
 }
