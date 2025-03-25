ca553a2931935ca731e98883b040316db012b1d6
aulouu <alina.minkovva@gmail.com>
2025-03-25 12:27:01 +0300

    delete java-errors

--- src/main/java/itmo/blps/exceptions/GlobalControllerExceptionHandler.java
+++ src/main/java/itmo/blps/exceptions/GlobalControllerExceptionHandler.java
@@ -16,120 +16,103 @@ public class GlobalControllerExceptionHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorResponse handleUserAlreadyExistException(UserAlreadyExistException e) {
-        return new ErrorResponse(e.getClass().getCanonicalName(),
-                e.getMessage());
+        return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleUserNotFoundException(UserNotFoundException e) {
-        return new ErrorResponse(e.getClass().getCanonicalName(),
-                e.getMessage());
+        return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public ErrorResponse handleUserNotAuthorizedException(UserNotAuthorizedException e) {
-        return new ErrorResponse(e.getClass().getCanonicalName(),
-                e.getMessage());
+        return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleOrderNotFoundException(OrderNotFoundException e) {
-        return new ErrorResponse(e.getClass().getCanonicalName(),
-                e.getMessage());
+        return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.FORBIDDEN)
     public ErrorResponse handleOrderNotBelongException(OrderNotBelongException e) {
-        return new ErrorResponse(e.getClass().getCanonicalName(),
-                e.getMessage());
+        return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorResponse handleNotValidOrderStatusException(NotValidOrderStatusException e) {
-        return new ErrorResponse(e.getClass().getCanonicalName(),
-                e.getMessage());
+        return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleProductNotFoundException(ProductNotFoundException e) {
-        return new ErrorResponse(e.getClass().getCanonicalName(),
-                e.getMessage());
+        return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorResponse handleProductIsOutOfStockException(ProductIsOutOfStockException e) {
-        return new ErrorResponse(e.getClass().getCanonicalName(),
-                e.getMessage());
+        return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleAddressNotFoundException(AddressNotFoundException e) {
-        return new ErrorResponse(e.getClass().getCanonicalName(),
-                e.getMessage());
+        return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleAddressNotProvidedException(AddressNotProvidedException e) {
-        return new ErrorResponse(e.getClass().getCanonicalName(),
-                e.getMessage());
+        return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorResponse handleAddressAlreadyExistsException(AddressAlreadyExistsException e) {
-        return new ErrorResponse(e.getClass().getCanonicalName(),
-                e.getMessage());
+        return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleCardNotFoundException(CardNotFoundException e) {
-        return new ErrorResponse(e.getClass().getCanonicalName(),
-                e.getMessage());
+        return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleCardNotProvidedException(CardNotProvidedException e) {
-        return new ErrorResponse(e.getClass().getCanonicalName(),
-                e.getMessage());
+        return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorResponse handleCardAlreadyExistsException(CardAlreadyExistsException e) {
-        return new ErrorResponse(e.getClass().getCanonicalName(),
-                e.getMessage());
+        return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.PAYMENT_REQUIRED)
     public ErrorResponse handleNotEnoughMoneyException(NotEnoughMoneyException e) {
-        return new ErrorResponse(e.getClass().getCanonicalName(),
-                e.getMessage());
+        return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     public ErrorResponse handleNotValidInputException(NotValidInputException e) {
-        return new ErrorResponse(e.getClass().getCanonicalName(),
-                e.getMessage());
+        return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleIllegalArgumentException(IllegalArgumentException e) {
-        return new ErrorResponse(e.getClass().getCanonicalName(),
-                e.getMessage());
+        return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
@@ -145,8 +128,7 @@ public class GlobalControllerExceptionHandler {
                         error.getRejectedValue() == null ? "пусто" : error.getRejectedValue()))
                 .collect(Collectors.toList());
 
-        return new ErrorResponse(
-                "Ошибка валидации",
-                String.join("\n", errors));
+        String errorMessage = "Ошибка валидации:\n" + String.join("\n", errors);
+        return new ErrorResponse(errorMessage);
     }
 }
