8c733acbb570a8ca479177f9306d5b2ccf458006
Uliana <uliana.shpineva@gmail.com>
2025-05-22 21:25:11 +0300

    camunda configs

--- src/main/java/itmo/blps/config/TransactionConfig.java
+++ src/main/java/itmo/blps/config/TransactionConfig.java
@@ -1,7 +1,7 @@
 package itmo.blps.config;
 
-//import com.atomikos.icatch.jta.UserTransactionManager;
-//import com.atomikos.jdbc.AtomikosDataSourceBean;
+import com.atomikos.icatch.jta.UserTransactionManager;
+import com.atomikos.jdbc.AtomikosDataSourceBean;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.engine.transaction.jta.platform.internal.AtomikosJtaPlatform;
 import org.postgresql.xa.PGXADataSource;
@@ -17,67 +17,84 @@ import org.springframework.transaction.jta.JtaTransactionManager;
 import javax.sql.DataSource;
 import java.util.Properties;
 
-//@Configuration
+@Configuration
 @RequiredArgsConstructor
 public class TransactionConfig {
     private final Environment environment;
 
-//    @Bean
-//    public DataSource dataSource() {
-//        PGXADataSource pgxaDataSource = new PGXADataSource();
-//        pgxaDataSource.setUrl(environment.getProperty("spring.jta.atomikos.datasource.orders-xa-ds.xa-properties.url"));
-//        pgxaDataSource.setUser(environment.getProperty("spring.jta.atomikos.datasource.orders-xa-ds.xa-properties.user"));
-//        pgxaDataSource.setPassword(environment.getProperty("spring.jta.atomikos.datasource.orders-xa-ds.xa-properties.password"));
-//
-//        AtomikosDataSourceBean atomikosDataSource = new AtomikosDataSourceBean();
-//        atomikosDataSource.setXaDataSource(pgxaDataSource);
-//        atomikosDataSource.setUniqueResourceName("ordersXADS");
-//        atomikosDataSource.setMinPoolSize(5);
-//        atomikosDataSource.setMaxPoolSize(20);
-//        return atomikosDataSource;
-//    }
+    @Bean
+    public DataSource dataSource() {
+        PGXADataSource pgxaDataSource = new PGXADataSource();
+        pgxaDataSource.setUrl(environment.getProperty("spring.jta.atomikos.datasource.orders-xa-ds.xa-properties.url"));
+        pgxaDataSource.setUser(environment.getProperty("spring.jta.atomikos.datasource.orders-xa-ds.xa-properties.user"));
+        pgxaDataSource.setPassword(environment.getProperty("spring.jta.atomikos.datasource.orders-xa-ds.xa-properties.password"));
 
-//    @Bean
-//    public JpaVendorAdapter jpaVendorAdapter() {
-//        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-//        adapter.setShowSql(true);
-//        adapter.setGenerateDdl(false);
-//        adapter.setDatabase(Database.POSTGRESQL);
-//        return adapter;
-//    }
+        AtomikosDataSourceBean atomikosDataSource = new AtomikosDataSourceBean();
+        atomikosDataSource.setXaDataSource(pgxaDataSource);
+        atomikosDataSource.setUniqueResourceName("ordersXADS");
+        atomikosDataSource.setMinPoolSize(5);
+        atomikosDataSource.setMaxPoolSize(20);
+        return atomikosDataSource;
+    }
 
-//    @Bean
-//    public LocalContainerEntityManagerFactoryBean entityManagerFactory(JpaVendorAdapter jpaVendorAdapter) {
-//
-//        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-//        em.setJtaDataSource(dataSource());
-//        em.setJpaVendorAdapter(jpaVendorAdapter);
-//        em.setPackagesToScan("itmo.blps.model");
-//
-//        Properties properties = new Properties();
-//        properties.put("hibernate.hbm2ddl.auto", "none");
-//        properties.put("hibernate.transaction.jta.platform", AtomikosJtaPlatform.class.getName());
-//        em.setJpaProperties(properties);
-//
-//        return em;
-//    }
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(false);
+        adapter.setDatabase(Database.POSTGRESQL);
+        return adapter;
+    }
 
-//    @Bean
-//    public UserTransactionManager userTransactionManager() {
-//        System.setProperty("com.atomikos.icatch.log_base_dir", "./atomikos-logs/orders-service");
-//        System.setProperty("com.atomikos.icatch.log_base_name", "tmlog-orders");
-//        System.setProperty("com.atomikos.icatch.tm_unique_name", "ordersTransactionManager");
-//
-//        UserTransactionManager userTransactionManager = new UserTransactionManager();
-//        userTransactionManager.setForceShutdown(false);
-//        return userTransactionManager;
-//    }
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(JpaVendorAdapter jpaVendorAdapter) {
 
-//    @Bean
-//    public JtaTransactionManager transactionManager() {
-//        JtaTransactionManager transactionManager = new JtaTransactionManager();
-//        transactionManager.setTransactionManager(userTransactionManager());
-//        transactionManager.setUserTransaction(userTransactionManager());
-//        return transactionManager;
-//    }
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setJtaDataSource(dataSource());
+        em.setJpaVendorAdapter(jpaVendorAdapter);
+        em.setPackagesToScan("itmo.blps.model");
+
+        Properties properties = new Properties();
+        properties.put("hibernate.hbm2ddl.auto", "none");
+        properties.put("hibernate.transaction.jta.platform", AtomikosJtaPlatform.class.getName());
+        em.setJpaProperties(properties);
+
+        return em;
+    }
+
+    @Bean
+    public UserTransactionManager userTransactionManager() {
+        System.setProperty("com.atomikos.icatch.log_base_dir", "./atomikos-logs/orders-service");
+        System.setProperty("com.atomikos.icatch.log_base_name", "tmlog-orders");
+        System.setProperty("com.atomikos.icatch.tm_unique_name", "ordersTransactionManager");
+
+        UserTransactionManager userTransactionManager = new UserTransactionManager();
+        userTransactionManager.setForceShutdown(false);
+        return userTransactionManager;
+    }
+
+    @Bean
+    public JtaTransactionManager transactionManager() {
+        JtaTransactionManager transactionManager = new JtaTransactionManager();
+        transactionManager.setTransactionManager(userTransactionManager());
+        transactionManager.setUserTransaction(userTransactionManager());
+        return transactionManager;
+    }
+
+
+    @Bean(name = "camundaDataSource")
+    public DataSource camundaDataSource() {
+        PGXADataSource pgxaDataSource = new PGXADataSource();
+        pgxaDataSource.setUrl(environment.getProperty("spring.jta.atomikos.datasource.orders-xa-ds.xa-properties.url"));
+        pgxaDataSource.setUser(environment.getProperty("spring.jta.atomikos.datasource.orders-xa-ds.xa-properties.user"));
+        pgxaDataSource.setPassword(environment.getProperty("spring.jta.atomikos.datasource.orders-xa-ds.xa-properties.password"));
+
+        AtomikosDataSourceBean atomikosDataSource = new AtomikosDataSourceBean();
+        atomikosDataSource.setXaDataSource(pgxaDataSource);
+        atomikosDataSource.setUniqueResourceName("camundaXADS"); // Уникальное имя для Camunda
+        atomikosDataSource.setMinPoolSize(5);
+        atomikosDataSource.setMaxPoolSize(20);
+        return atomikosDataSource;
+    }
+
 }
